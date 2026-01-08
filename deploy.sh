d10b85c988795eaec40136b6cbfd9ccf07664db1
ZAHFOUF YOUNES <youneszahfouf@gmail.com>
2026-01-08 11:31:46 +0100

    github action and reslove

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ whoami
 CONFIG_FILE="/deploy/apps.config"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-  echo "ÔØî Config file not found: $CONFIG_FILE"
+  echo " Config file not found: $CONFIG_FILE"
   exit 1
 fi
 
@@ -13,53 +13,53 @@ fi
 source "$CONFIG_FILE"
 
 if [ -z "${APPS[*]}" ]; then
-  echo "ÔØî No applications defined in config"
+  echo " No applications defined in config"
   exit 1
 fi
 
 if [ -z "$BRANCH" ]; then
-  echo "ÔØî BRANCH is not defined in config"
+  echo " BRANCH is not defined in config"
   exit 1
 fi
 
-echo "­ƒÜÇ Starting deploy on branch: $BRANCH"
-echo "­ƒôü Apps loaded from: $CONFIG_FILE"
+echo " Starting deploy on branch: $BRANCH"
+echo " Apps loaded from: $CONFIG_FILE"
 
 for DIR in "${APPS[@]}"; do
   echo "=============================="
-  echo "­ƒôª Deploying: $DIR"
+  echo " Deploying: $DIR"
 
   if [ ! -d "$DIR/.git" ]; then
-    echo "ÔØî Not a git repository: $DIR"
+    echo " Not a git repository: $DIR"
     exit 1
   fi
 
   cd "$DIR"
 
-  echo "­ƒöä git pull origin $BRANCH"
+  echo " git pull origin $BRANCH"
   git pull origin "$BRANCH"
 
   CACHE_DIR="$DIR/var/cache/prod"
   if [ -d "$CACHE_DIR" ]; then
-    echo "­ƒº╣ Clearing cache: $CACHE_DIR"
+    echo " Clearing cache: $CACHE_DIR"
     rm -rf "$CACHE_DIR"
   else
-    echo "Ôä╣´©Å Cache directory not found: $CACHE_DIR"
+    echo " Cache directory not found: $CACHE_DIR"
   fi
 
-  echo "Ô£à Done: $DIR"
+  echo "Done: $DIR"
 
   CACHE_DIR="$DIR/var/cache/bash"
   if [ -d "$CACHE_DIR" ]; then
-    echo "­ƒº╣ Clearing cache BASH: $CACHE_DIR"
+    echo "Clearing cache BASH: $CACHE_DIR"
     rm -rf "$CACHE_DIR"
   else
-    echo "Ôä╣´©Å Cache directory not found: $CACHE_DIR"
+    echo "Cache directory not found: $CACHE_DIR"
   fi
 
-  echo "Ô£à Done bash : $DIR"
+  echo "Done bash : $DIR"
 
 
 done
 
-echo "­ƒÄë All apps deployed successfully"
+echo "­All apps deployed successfully"
